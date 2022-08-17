cfe23df0db0026f6ef1428845b3933c53eeeb3a8
Mariia-Kravtsova <108951561+Mariia-Kravtsova@users.noreply.github.com>
2022-08-17 21:29:49 +0300

    Display the forecast data using the api

--- src/app.js
+++ src/app.js
@@ -21,32 +21,39 @@ let now = new Date();
 let h2=document.querySelector("#current-date");
 h2.innerHTML = formatDate(now);
 
+function formatDay(timestamp) {
+    let date = new Date (timestamp * 1000);
+    let day = date.getDay();
+let days = ["Sun", "Mon","Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
     let forecastElement=document.querySelector("#forecast");
      let forecastHTML = `<div class="row">`;
-let days = ["Thu", "Fri", "Sat", "Sun"];
-days.forEach(function(day) {
-      forecastHTML = forecastHTML + `
+
+forecast.forEach(function(forecastDay, index) {
+    if (index < 6) {  
+    forecastHTML = forecastHTML + `
     
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
-          
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+       
           <img
-          src="http://openweathermap.org/img/wn/10d@2x.png"
-          alt="light rain with sun"
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
           width="55"
           />
           <div class="weather-forecast-temperatures">
             <span class="weather-forecast-temperature-max">
-              18° </span>
+              ${Math.round(forecastDay.temp.max)}° </span>
              <span class="weather-forecast-temperature-min">
-              12°
+              ${Math.round(forecastDay.temp.min)}° 
             </span>
           </div>
        
       </div>`;
-
+    }
 })
       forecastHTML=forecastHTML +'</div>';
     forecastElement.innerHTML = forecastHTML;
@@ -113,5 +120,4 @@ let fahrenheitLink=document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 let celsiusLink=document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
-
 
